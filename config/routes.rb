b2ecf39086aa4fbd02c6893ecfc1e3fcf99328c5
thaker-nish <thaker.nish@gmail.com>
2016-12-27 14:06:27 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'pages#home'
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  get '/users/:id' => 'users#show'
+
+  get '/promoters' => 'promoters#index'
+  get '/promoters_signup' => 'promoters#new'
+  post '/promoters' => 'promoters#create'
+  get '/promoters_login' => 'promoters_sessions#new'
+  post '/promoters_login' => 'promoters_sessions#create'
+  get '/promoters_logout' => 'promoters_sessions#destroy'
+  get '/promoters/:id' => 'promoters#show'
+
+  get '/clubs' => 'clubs#index'
+  get '/clubs/:id' => 'clubs#show'
 end
