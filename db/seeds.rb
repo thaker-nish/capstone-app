38af64e8eb7e36d16185892219ee82bcb10d25c4
thaker-nish <thaker.nish@gmail.com>
2017-01-04 19:06:50 -0600

    Add bcrypt to gem file and create and run seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,57 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+clubs = ["LIV", "Story", "Nikki Beach"]
+clubs.each do |club|
+  club = Club.create(name: club,
+    email: "info@#{club}.com",
+    phone:"number")
+end
+promoters = ["Jake Johnson", "Melissa Jones", "Luke James"]
+promoters.each do |promoter|
+  promoter = Promoter.create(name: promoter,
+    email: "#{promoter}@gmail.com",
+    phone:"number",
+    password:"password")
+end
+
+
+users = ["John Doe", "Sara Blake", "Joe Smoe"]
+
+users.each do |user|
+  user = User.create(name: user,
+    email: "#{user}@gmail.com",
+    phone:"number",
+    password:"password")
+end
+
+users = User.all
+promoters = Promoter.all
+
+users.each do |user|
+  follower = PromoterFollower.create(
+      user_id: user.id,
+      promoter_id: [1,2,3].sample)
+end
+
+clubs = Club.all
+promoters = Promoter.all
+
+clubs.each do |club|
+  ClubPromoter.create(club_id: club.id,
+    promoter_id: [1,2,3].sample)
+end
+
+promotions = ["Free drinks", "No cover", "Half priced bottles"]
+promotions.each do |promotion|
+  promotion = Promotion.create(name: promotion)
+end
+
+promotions = Promotion.all
+clubs = Club.all
+users = User.all
+promoters = Promoter.all
+
+promoters.each do |promoter|
+  promoter = Guestlist.create(club_id: promoter.clubs.ids.sample ,
+    promoter_id: promoters.ids.sample ,
+    user_id: promoter.users.ids.sample ,
+    promotion_id: [1,2,3].sample )
+end
